5e857f1ae34470690e2da8c73a3d40ca465e9149
bhagyasrijyothula <155608309+bhagyasrijyothula@users.noreply.github.com>
2025-10-29 10:44:13 +0530

    Time: 1266 ms (24.75%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1174-sales-analysis-iii/sales-analysis-iii.sql
@@ -0,0 +1,1 @@
+select s.product_id,p.product_name from product p join sales s on p.product_id= s.product_id group by product_id, product_name having min(s.sale_date)>= '2019-01-01' and max(s.sale_date) <= '2019-03-31';
